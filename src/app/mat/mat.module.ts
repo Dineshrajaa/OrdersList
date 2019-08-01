f5a53625eb3843f724c43bd1d9ea73fd1424b06b
Dinesh Raja <dineshkumar.r09@ad.infosys.com>
2019-08-01 19:15:57 +0530

    Add forms

--- src/app/mat/mat.module.ts
+++ src/app/mat/mat.module.ts
@@ -3,31 +3,34 @@ import { CommonModule } from '@angular/common';
 
 import {
   MatToolbarModule, MatListModule,
-  MatButtonModule, MatDialogModule,
-  MatGridListModule, MatIconModule, MatInputModule
+  MatButtonModule, MatDialogModule, MatDatepickerModule,
+  MatGridListModule, MatIconModule, MatInputModule, MatNativeDateModule
 } from '@angular/material';
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
+    MatDatepickerModule,
     MatDialogModule,
     MatGridListModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatNativeDateModule,
     MatToolbarModule
   ],
   exports: [
     MatButtonModule,
+    MatDatepickerModule,
     MatDialogModule,
     MatGridListModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatNativeDateModule,
     MatToolbarModule
-  ]
+  ], providers: [MatDatepickerModule]
 })
 export class MatModule { }
--- src/app/shared/constants/error-messages.ts
+++ src/app/shared/constants/error-messages.ts
@@ -1,4 +1,7 @@
 export const ErrorMessages = {
     'origin_required': 'Please enter origin',
-    'destination_required': 'Please enter destination'
+    'destination_required': 'Please enter destination',
+    'businessUnit_required': 'Please select a business unit',
+    'pickupDate_required': 'Please choose a pickup date',
+    'deliveryDate_required': 'Please choose a delivery date'
 };
